d5e92fdb82eda5775d8cdfa1e7431e718e5d9247
isabella <isabella.salomao@usp.br>
2021-07-24 22:34:46 -0300

    Add new color pallete in CSS, modify logo according to new colors. Include some icons for buttons

--- src/screens/homepage.jsx
+++ src/screens/homepage.jsx
@@ -3,11 +3,14 @@ import { Button } from 'react-bootstrap'
 const Homepage = () => (
 <div className="container">
     <div className="header">
-        <img src="./images/vacinow1.png" />
+        <a href="/">
+            <img src="../images/vacinow3.png" alt="vacinow-logo" className="logo"/>
+        </a>
+        <hr />
     </div>
 
     <div className="description">
-        <h1 className="mainText">Cadastre uma vacina:</h1>
+        <h1 className="title">Cadastre uma vacina:</h1>
     </div>
 
     <div className="main">
@@ -15,15 +18,14 @@ const Homepage = () => (
             {/* botão de upload */}
             <h2 className="mainText">Faça upload do seu formulário scanneado</h2>
             {/* <Button className={styles.linkButton} href="/upload">Login</Button> */}
-            <Button className="linkButton" href="/upload">Upload</Button>
-            
+            <Button className="linkButton" href="/upload"> <img src="../images/upload-big-arrow.png" alt="upload-icon" className="icon"/><p>Upload</p></Button>
+
         </div>
-        <div> <p className="mainText"> OU </p> </div>
         <div className="links">
-            {/* botão de cadastro  */}
+            {/* botão de cadastro */}
             <h2 className="mainText"> Preencha o formulário online</h2>
             {/* <Button className={styles.linkButton} href="/upload">Login</Button> */}
-            <Button style={{ marginTop: 52 }} className="linkButton" href="/registration">Preencher</Button>
+            <Button style={{ marginTop: 52 }} className="linkButton" href="/registration"><img src="../images/contact-form.png" alt="form-icon" className="icon"/><p>Preencher</p> </Button>
         </div>
     </div>
 </div>
