24c7e62b8245d6f5df922908e0877151373f90de
isabella <isabella.salomao@usp.br>
2021-07-24 22:36:27 -0300

    Include header in all pages. Correct links

--- src/screens/upload.jsx
+++ src/screens/upload.jsx
@@ -3,60 +3,60 @@ import { Button } from 'react-bootstrap'
 import api from 'axios'
 
 const Upload = () => {
-    const [image, setImage] = useState()
+const [image, setImage] = useState()
 
-    function handleSubmit(event) {
-        event.preventDefault()
+function handleSubmit(event) {
+event.preventDefault()
 
-        if (!image) {
-            return
-        }
-        const data = new FormData()
+if (!image) {
+return
+}
+const data = new FormData()
 
-        data.append('image', image)
+data.append('image', image)
 
-        api.post('/readfile', data)
-            .then(function (response) {
-                alert(response.data.text)
-            })
-            .catch(function (error) {
-                console.log(error)
-            })
-    }
+api.post('/readfile', data)
+.then(function (response) {
+alert(response.data.text)
+})
+.catch(function (error) {
+console.log(error)
+})
+}
 
-    function handleImage(event) {
-        if (!event.target.files) {
-            return
-        } else {
-            // src = URL.createObjectURL(event.target.files[0])
-        }
-        setImage(event.target.files[0])
-    }
-    return (
-        <div className="container">
-            <h1 className="title">Vacinow</h1>
-            <br />
-            <hr />
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <h2>Fala upload do formulário scanneado: </h2>
-                    <label htmlFor="image" className="upload">Selecionar imagem</label>
-                    <input
-                        onChange={handleImage}
-                        type="file"
-                        accept="image/*;capture=camera"
-                        id="image"
-                        style={{ display: "none" }}
-                    />
-                    <Button type="submit" className="linkButton">Enviar</Button>
-                </div>
+function handleImage(event) {
+if (!event.target.files) {
+return
+} else {
+// src = URL.createObjectURL(event.target.files[0])
+}
+setImage(event.target.files[0])
+}
+return (
+<div className="container">
+    {/* todo:  Deveria ser replicado só né, fiz hardcodado mesmo */}
+    <div className="header">
+        <a href="/">
+            <img src="../images/vacinow3.png" alt="vacinow-logo" className="logo" />
+        </a>
+        <hr />
+    </div>
 
-            </form>
-            <div>
-                <h2>Ou então preencha o formulário diretamente: </h2>
-                <Button href="/registrationForm" className="linkButton">Preencher Formulário</Button>
-            </div>
+    <form onSubmit={handleSubmit}>
+        <div>
+            <h2>Fala upload do formulário scanneado: </h2>
+            <label htmlFor="image" className="upload">Selecionar imagem</label>
+            <input onChange={handleImage} type="file" accept="image/*;capture=camera" id="image"
+                style={{ display: "none" }} />
+            <Button type="submit" className="linkButton">Enviar</Button>
         </div>
-    )
+
+    </form>
+    <div>
+        <h2>Ou então preencha o formulário diretamente: </h2>
+        <Button href="/registration" className="linkButton">Preencher Formulário</Button>
+    </div>
+</div>
+)
 }
 export default Upload
