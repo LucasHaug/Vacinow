97cd054ddab7e6c5890b4e8f783d4e9c90b50790
Renzo Abensur <renzo.abensur@radixeng.com.br>
2021-07-28 21:44:07 -0300

    :fire: Remove checkPage
    Co-authored-by: Lucas Haug <LucasHaug@users.noreply.github.com>
    Co-authored-by: Renato Freitas <freitas-renato@outlook.com>
    Co-authored-by: IsaBologna <isabella.salomao@usp.br>

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import './styles/global.css'
 import Homepage from './screens/homepage';
 import Upload from './screens/UploadScreen/uploadScreen';
 import Registration from './screens/registrationForm/registrationForm';
-import CheckPage from './screens/checkPage';
 
 function App() {
   return (
@@ -15,7 +14,6 @@ function App() {
         <Route path='/' exact component={Homepage} />
         <Route path='/upload' exact component={Upload} />
         <Route path='/registration' exact component={Registration} />
-        <Route path='/checkpage' exact component={CheckPage} />
       </Switch>
     </Router>
   );
